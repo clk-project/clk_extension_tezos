8fdd394dfc3165d5aa45b0476c6a92c1ccc4515d
Samuel Loury <konubinixweb@gmail.com>
2021-09-22 11:02:28 +0200

    First commit

---
+++ python/tezos.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import base64
+
+import base58
+import pysodium
+from clk.decorators import argument, group
+from clk.log import get_logger
+from pyblake2 import blake2b
+
+LOGGER = get_logger(__name__)
+
+
+def tb(data):
+    return b"".join(map(lambda x: x.to_bytes(1, "big"), data))
+
+
+@group()
+def tezos():
+    "Commands to play with tezos"
+
+
+@tezos.command()
+@argument("publickeyb64", help="The base64 representation of the public key")
+def compute_address(publickeyb64):
+    "Compute some tezos address, provided the public key"
+    public_key = base64.b64decode(publickeyb64.encode())
+    pkh = blake2b(data=public_key, digest_size=20).digest()
+    # tz1
+    print(base58.b58encode_check(tb([6, 161, 159]) + pkh).decode())
+
+
+def encode_signature(message):
+    # sig
+    return base58.b58encode_check(tb([4, 130, 43]) + message).decode()
+
+
+def digest(messageb64):
+    message = base64.b64decode(messageb64.encode())
+    return base64.b64encode(pysodium.crypto_generichash(message)).decode()
+
+
+@tezos.command()
+@argument("publickeyb64", help="The base64 representation of the public key")
+def compute_public_key(publickeyb64):
+    "Compute the public key in a format that tezos understands"
+    public_key = base64.b64decode(publickeyb64.encode())
+    print(base58.b58encode_check(tb([13, 15, 37, 217]) + public_key).decode())
+
+
+@tezos.command()
+@argument("messageb64", help="The message encoded in base64")
+def _digest(messageb64):
+    """Compute the digest to sign, provided the (forged) message"""
+    print(digest(messageb64))
